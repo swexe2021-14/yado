b44faf4caa7778d77908436c91ba46cc3134632d
fukeriko <1911140049j@ed.fuk.kindai.ac.jp>
2021-11-22 09:01:11 +0000

    hotels諸々追加

--- yado2/app/controllers/hotels_controller.rb
+++ yado2/app/controllers/hotels_controller.rb
@@ -1,2 +1,25 @@
 class HotelsController < ApplicationController
+    def index
+        @hotels = Hotel.all
+    end
+    
+    def new
+        @hotel = Hotel.new
+    end
+    
+    def create
+        place = params[:hotel][:place]
+        hname = params[:hotel][:hname]
+        comment = params[:hotel][:comment]
+        price = params[:hotel][:price]
+        hotel = Hotel.new(place: place, hname: hname, comment: comment, price: price)
+        hotel.save
+        redirect_to root_path
+    end
+    
+    def destroy
+        hotel = Hotel.find(params[:id])
+        hotel.destroy
+        redirect_to root_path
+    end
 end
--- yado2/app/controllers/tourists_controller.rb
+++ yado2/app/controllers/tourists_controller.rb
@@ -1,2 +1,30 @@
 class TouristsController < ApplicationController
+    def index
+    @tourists = Tourist.all
+  end
+
+  def new
+    @tourist = Tourist.new
+  end
+
+  def create 
+    @tourist = Tourist.new(
+    name: params[:tourist][:name],
+    pass: params[:tourist][:pass],
+    pass_confirmation: params[:tourist][:pass_confirmation])
+    if @user.valid?
+      flash[:notice] = '追加しました'
+      @tourist.pass = BCrypt::Pass.create(params[:tourist][:pass])
+      @tourist.save
+      redirect_to tourists_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    tourist = Tourist.find(params[:id])
+    tourist.destroy
+    redirect_to tourists_path
+  end
 end
