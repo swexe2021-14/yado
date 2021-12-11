8062692f425d98fbaefb6faf329cea2b2d6b7d87
fukeriko <1911140049j@ed.fuk.kindai.ac.jp>
2021-12-11 14:33:52 +0000

    reviewのフォーム作成

--- yado2/config/routes.rb
+++ yado2/config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     get 'get_tourist/:name', to: 'tourist#get_tourist'
     resources :hotels
     get 'get_image/:id', to: 'hotels#get_image'
+    resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
