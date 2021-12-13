d8f5390612def0ef92e4ca440c826fc41ffc1925
fukeriko <1911140049j@ed.fuk.kindai.ac.jp>
2021-12-13 07:51:09 +0000

    レビュー機能の完成

--- yado2/app/controllers/application_controller.rb
+++ yado2/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+private
+    def current_tourist
+        if session[:login_name]
+            tourist = Tourist.find_by(name: session[:login_name])
+        else
+            tourist = Tourist.create
+            session[:lonin_name] = tourist.name
+        end
+        tourist
+    end
+    helper_method :current_tourist
 end
